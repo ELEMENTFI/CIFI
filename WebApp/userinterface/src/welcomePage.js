51db4d3d22f71449f771176d6787327b2e7e3d72
shyam03 <shyamganeshravichandran@gmail.com>
2021-03-22 19:35:45 +0530

    commit on main

--- WebApp/userinterface/src/welcomePage.js
+++ WebApp/userinterface/src/welcomePage.js
@@ -3,7 +3,9 @@ import React from 'react';
 function Welcome(){
     return (
         <div>
+            <center>
             <b>Welcome to React</b>
+            </center>
         </div>
     )
 }
