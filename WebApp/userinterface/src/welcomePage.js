b902f099b4d8d7ba41686ae5cf536b4ea6344889
shyam03 <shyamganeshravichandran@gmail.com>
2021-04-20 19:38:09 +0530

    commit

--- WebApp/userinterface/src/welcomePage.js
+++ WebApp/userinterface/src/welcomePage.js
@@ -5,7 +5,8 @@ function Welcome(){
         <div>
             
            <marquee><b>Welcome to React Native App</b></marquee>
-           <p>It Uses Javascript For Making Native Mobilr App</p>
+           <p>It Uses Javascript For Making Native Mobile App</p>
+           <p>It is similar to Reactjs</p>
         </div>
     )
 }
