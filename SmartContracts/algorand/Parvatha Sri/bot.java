2af7f501aefedf076940308fc580d62865ec6daa
sriparvatha <priyanila2412@gmail.com>
2021-05-12 06:46:09 +0530

    bot_update

---
+++ SmartContracts/algorand/Parvatha Sri/bot.java
@@ -0,0 +1,7 @@
+
+$requestFactory = new Http\Factory\Guzzle\RequestFactory()
+$streamFactory = new Http\Factory\Guzzle\StreamFactory();
+$client = new Http\Adapter\Guzzle6\Client();
+$apiClient = new \TgBotApi\BotApiBase\ApiClient($requestFactory, $streamFactory, $client);
+$bot = new \TgBotApi\BotApiBase\BotApiComplete($botKey, $apiClient, new \TgBotApi\BotApiBase\BotApi\BotApiNormalizer());
+$bot->sendMessage(\TgBotApi\BotApiBase\Method\SendMessageMethod::create($userId, 'Hi'));
