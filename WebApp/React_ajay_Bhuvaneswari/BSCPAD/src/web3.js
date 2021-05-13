1dc923c8c4e800542dbcc66347877a9d22f5b321
Bhuvaneswari13 <bhuvantnj13@gmail.com>
2021-05-13 20:50:11 +0530

    BSCPAD5

--- WebApp/React_ajay_Bhuvaneswari/BSCPAD/src/web3.js
+++ WebApp/React_ajay_Bhuvaneswari/BSCPAD/src/web3.js
@@ -1,6 +1,6 @@
 import Web3 from 'web3';
 // ADDITION This is needed to allow this application to interact with metamask
-window.alert("Do you want to connect with metamask");
+//window.alert("Do you want to connect with metamask");
 window.ethereum.enable();
 // window.web3 coming from metamask
 const currProvider = window.web3.currentProvider
