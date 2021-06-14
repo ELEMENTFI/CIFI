60ce0f4a5199e3b15b5f03f8e556f96226092ba6
Bhuvaneswari13 <bhuvantnj13@gmail.com>
2021-06-14 20:47:48 +0530

    BSCPAD26

--- WebApp/React_ajay_Bhuvaneswari/BSCPAD/src/fourthpage.js
+++ WebApp/React_ajay_Bhuvaneswari/BSCPAD/src/fourthpage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+//Calling Bootstrap 4.5 css
 import 'bootstrap/dist/css/bootstrap.min.css';
 import fireDB from'./firebase';
 class fourthpage extends React.Component {
@@ -11,13 +12,15 @@ class fourthpage extends React.Component {
       }
       componentDidMount() {
    
-        fireDB.database().ref(" ").on("value", snapshot => {
+   
+     
+        fireDB.database().ref("Con").on("value", snapshot => {
           let Con = [];
           snapshot.forEach(snap => {
               // snap.val() is the dictionary with all your keys/values from the 'students-list' path
               Con.push(snap.val());
           });
-          this.setState({ : Con });
+          this.setState({ Con: Con });
         });
       
       
@@ -36,16 +39,16 @@ class fourthpage extends React.Component {
         
         <div className='row'> 
         <div className='col-xl-12'>
-        { this.state..map(data => 
+        { this.state.Con.map(data => 
             <div className="card float-left bg-sky text-dark" style={{width: '18rem', marginRight: '1rem', marginBlockStart: '1rem' }}>
               <div className="card-body">
-                <h6 className="card-title">At : { data.At }</h6>
-                <h6 className="card-text">Balance : { data.Balance }</h6>
-                <h6 className="card-text">Decimal : { data.Decimal }</h6>
-                <h6 className="card-text">Name : { data.Name }</h6>
-                <h6 className="card-text">No : { data.No }</h6>
-                <h6 className="card-text">Symbol : { data.Symbol }</h6>
-                <h6 className="card-text">Total Supply : { data.ggg }</h6>
+                <h5 className="card-title">At : { data.At }</h5>
+                <h5 className="card-text">Balance : { data.Balance }</h5>
+                <h5 className="card-text">Decimal : { data.Decimal }</h5>
+                <h5 className="card-text">Name : { data.Name }</h5>
+                <h5 className="card-text">No : { data.No }</h5>
+                <h5 className="card-text">Symbol : { data.Symbol }</h5>
+                <h5 className="card-text">Total Supply : { data.ggg }</h5>
                 </div> 
             </div> 
             )
@@ -58,3 +61,5 @@ class fourthpage extends React.Component {
   }
   }
   export default fourthpage;
+
+
