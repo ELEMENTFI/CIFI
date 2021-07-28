aca55eead75a1a72005e8695ebf03209970aa1a9
ramBoson <ramamugesh95@gmail.com>
2021-07-28 10:12:53 +0530

    add crypter in cifi repo

---
+++ WebApp/Crypter/src/App.js
@@ -0,0 +1,135 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./styles/app.sass";
+import Page from "./components/Page";
+import Home from "./screens/Home";
+import UploadVariants from "./screens/UploadVariants";
+import UploadDetails from "./screens/UploadDetails";
+import ConnectWallet from "./screens/ConnectWallet";
+import Faq from "./screens/Faq";
+import Activity from "./screens/Activity";
+import Search01 from "./screens/Search01";
+import Search02 from "./screens/Search02";
+import Profile from "./screens/Profile";
+import ProfileEdit from "./screens/ProfileEdit";
+import Item from "./screens/Item";
+import PageList from "./screens/PageList";
+
+function App() {
+  return (    
+    <Router>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <Page>
+              <Home />
+            </Page>
+          )}
+        />
+        <Route
+          exact
+          path="/upload-variants"
+          render={() => (
+            <Page>
+              <UploadVariants />
+            </Page>
+          )}
+        />
+        <Route
+          exact
+          path="/upload-details"
+          render={() => (
+            <Page>
+              <UploadDetails />
+            </Page>
+          )}
+        />
+        <Route
+          exact
+          path="/connect-wallet"
+          render={() => (
+            <Page>
+              <ConnectWallet />
+            </Page>
+          )}
+        />
+        
+        <Route
+          exact
+          path="/faq"
+          render={() => (
+            <Page>
+              <Faq />
+            </Page>
+          )}
+        />
+        <Route
+          exact
+          path="/activity"
+          render={() => (
+            <Page>
+              <Activity />
+            </Page>
+          )}
+        />
+        <Route
+          exact
+          path="/search01"
+          render={() => (
+            <Page>
+              <Search01 />
+            </Page>
+          )}
+        />
+        <Route
+          exact
+          path="/search02"
+          render={() => (
+            <Page>
+              <Search02 />
+            </Page>
+          )}
+        />
+        <Route
+          exact
+          path="/profile"
+          render={() => (
+            <Page>
+              <Profile />
+            </Page>
+          )}
+        />
+        <Route
+          exact
+          path="/profile-edit"
+          render={() => (
+            <Page>
+              <ProfileEdit />
+            </Page>
+          )}
+        />
+        <Route
+          exact
+          path="/item"
+          render={() => (
+            <Page>
+              <Item />
+            </Page>
+          )}
+        />
+        <Route
+          exact
+          path="/pagelist"
+          render={() => (
+            <Page>
+              <PageList />
+            </Page>
+          )}
+        />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
---
+++ WebApp/Crypter/src/components/Header/index.js
@@ -0,0 +1,176 @@
+import React, { useState,useEffect } from "react";
+import { Link } from "react-router-dom";
+//, NavLink
+import cn from "classnames";
+import styles from "./Header.module.sass";
+import Icon from "../Icon";
+import Image from "../Image";
+import Notification from "./Notification";
+import User from "./User";
+
+const nav = [
+  {
+    url: "/search01",
+    title: "Discover",
+    onclick:()=>{
+      console.log("profile")
+    }
+  },
+  {
+    url: "/faq",
+    title: "How it work",
+  },
+  {
+    url: "/item",
+    title: "Create item",
+  },
+  {
+    url: "/profile",
+    title: "Profile",
+    onclick:()=>{
+      console.log("profile")
+    }
+  },
+];
+
+const Headers = () => {
+  const [visibleNav, setVisibleNav] = useState(false);
+  const [search, setSearch] = useState("");
+  const [connected, setConnected] = useState(false);
+  
+
+  const handleSearch = (e) => {
+    console.log("inside search bar function")
+  };
+
+  const isconnected=()=>{
+
+    //check
+
+    
+    let get=localStorage.getItem("wallet");
+    let getalgo=localStorage.getItem("walletalgo");
+    console.log("walletcheck",get)
+    console.log("walletcheckalgo",getalgo)
+
+    if(get)
+    setConnected(true)
+    // else if(getalgo)
+    // setConnected(true)
+    else
+    setConnected(false)
+    
+  
+  }
+
+  useEffect(() => {isconnected()}, [])
+
+  const onProfile=()=>{
+    console.log("hello profile")
+    //setIsOpen(false);
+    //history.push("/")
+  }
+
+  return (
+    
+    <header className={styles.header}>
+      
+      <div className={cn("container", styles.container)}>
+
+      
+        {/* <button type="submit" onClick={()=>{setConnected(!connected)}}>Toggle</button> */}
+        <Link className={styles.logo} to="/">
+          <Image
+            className={styles.pic}
+            src="/images/logo-dark.png"
+            srcDark="/images/logo-light.png"
+            alt="Fitness Pro"
+          />
+        </Link>
+        <div className={cn(styles.wrapper, { [styles.active]: visibleNav })}>
+          
+          <nav className={styles.nav}>
+            {nav.map((x, index) => (
+              <Link
+                className={styles.link}
+                // activeClassName={styles.active}
+                to={x.url}
+                key={index}
+              >
+                {x.title}
+              </Link>
+            ))}
+          </nav>
+
+          {/* searchbar here */}
+          <form
+            className={styles.search}
+            action=""            
+            // onSubmit={() => handleSubmit()}
+          >
+            <input
+              className={styles.input}
+              type="text"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              name="search"
+              placeholder="Search"
+              required
+            />
+            
+            <button className={styles.result} onClick={() => handleSearch}>
+              <Icon name="search" size="20" />
+            </button>
+          </form>
+
+
+          {/* <Link
+            className={cn("button-small", styles.button)}
+            to="/upload-variants"
+          >
+            Upload
+          </Link> */}
+        </div>
+
+        {/* Notification button below */}          
+        <Notification className={styles.notification} />
+        <Link
+          className={cn("button-small", styles.button)}
+          to="/upload-variants"
+        >
+          Upload
+        </Link>
+        
+
+{
+  connected ? 
+
+  
+  <Link
+  className={cn("button-small", styles.button)}
+  to="/connect-wallet"
+>
+Connected
+</Link>
+:
+       <Link
+          className={cn("button-stroke button-small", styles.button)}
+          to="/connect-wallet"
+        >
+          Connect Wallet          
+        </Link>
+
+}        
+
+        <User className={styles.user} onProfile={()=>onProfile}/>
+        
+        <button
+          className={cn(styles.burger, { [styles.active]: visibleNav })}
+          onClick={() => setVisibleNav(!visibleNav)}
+        ></button>
+      </div>
+    </header>
+  );
+};
+
+export default Headers;
